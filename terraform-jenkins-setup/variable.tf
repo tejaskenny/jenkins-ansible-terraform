ed1b600b58bb8d1c24c2ee71f0d1775879301377
Tejas Kenny <90190490+tejaskenny@users.noreply.github.com>
2025-08-30 11:01:18 +0530

    Add files via upload

---
+++ terraform-jenkins-setup/variable.tf
@@ -0,0 +1,58 @@
+
+variable "vpc_id" {
+  description = "The ID of the VPC to use"
+  type        = string
+  default     = "vpc-08acb8e378d7416ac"
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the instances will be launched"
+  type        = string
+  #default     = "subnet-00b43076033f05567" # private subnet id
+  default = "subnet-02d5c7feadf78aba4" ## public subnet id
+}
+
+variable "instance_type" {
+  description = "A list of instance types to use for each instance"
+  type        = list(string)
+  default     = ["t3.micro"]
+}
+
+variable "private_ips" {
+  description = "A list of private IPs for the instances"
+  type        = list(string)
+  default     = ["172.32.1.21"]
+}
+
+variable "instance_names" {
+  description = "A list of names for the instances"
+  type        = list(string)
+  default     = ["cr-dr-jenkins-server"]
+}
+
+variable "ami_id" {
+  description = "The ID of the AMI to use for the instance"
+  type        = string
+  #default     = "ami-0bcdb47863b39579f"
+  default = "ami-003932de22c285676"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use"
+  type        = string
+  default     = "DRkey"
+}
+
+
+
+variable "security_group_id" {
+  description = "The ID of the existing security group"
+  type        = string
+  default     = "sg-02080a7b627d9ea68" # Replace with your security group ID
+}
